e6409a0377e1a284a6d003483b6717d0cee3aee8
Diana Dsouza <dianadsouza42@gmail.com>
2024-05-29 11:44:44 +0530

    testing1

---
+++ for.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    # YOUR CODE GOES HERE
+    for i in range(n):
+        if i % 2 != 0:
+            print(i)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
